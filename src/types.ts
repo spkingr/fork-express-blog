b086edf2c56855de247d3d9765dd3e21b77c0580
seemrcola <seemrcola@163.com>
2023-06-14 20:15:11 +0800

    fix: .user bug fix

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,8 @@
-import type { NextFunction, Request, Response } from 'express'
+import type { NextFunction, Response } from 'express'
+import type { BlogRequest } from '../types/index.js'
 
 // 定义一个全局Middleware类型
-export type Middleware = (req: Request, res: Response, next: NextFunction) => void
+export type Middleware = (req: BlogRequest, res: Response, next: NextFunction) => void
 
 // 定义一个全局Error Middleware类型
-export type ErrorMiddleware = (err: any, req: Request, res: Response, next: NextFunction) => void
+export type ErrorMiddleware = (err: any, req: BlogRequest, res: Response, next: NextFunction) => void
